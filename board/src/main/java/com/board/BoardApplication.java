9a825914da76607933dc2caf09ca76216ba6ec24
plzballpen <plzballpen@gmail.com>
2018-05-16 17:49:08 +0900

    2-mapper까지

--- board/src/main/java/com/board/BoardApplication.java
+++ board/src/main/java/com/board/BoardApplication.java
@@ -1,12 +1,27 @@
 package com.board;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BoardApplication {
-
+//링크 = http://private.tistory.com/36
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
+	
+	/**
+	 *  SqlSessionFactory 설정
+	 */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource)throws Exception{
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        return sessionFactory.getObject();
+    }
 }
--- board/src/test/java/com/board/BoardApplicationTests.java
+++ board/src/test/java/com/board/BoardApplicationTests.java
@@ -1,16 +1,34 @@
 package com.board;
+ 
+import java.sql.Connection;
+
+import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+ 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.MOCK)
 public class BoardApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+    
+    @Autowired
+    private DataSource ds; //작성
+ 
+    @Test
+    public void contextLoads() {
+    }
+ 
+    @Test
+    public void testConnection() throws Exception{ //작성
+        System.out.println("ds : "+ds);
+        
+        Connection con = ds.getConnection(); //ds(DataSource)에서 Connection을 얻어내고
+        
+        System.out.println("con : "+con); //확인 후
+        
+        con.close(); //close
+    }
 }
