ad3e17587b328d2d6e6b4eaba4ab548371c39d39
plzballpen <plzballpen@gmail.com>
2018-05-17 13:20:56 +0900

    View에 띄우는 것까지

--- board/src/main/java/com/board/mapper/BoardMapper.java
+++ board/src/main/java/com/board/mapper/BoardMapper.java
@@ -1,8 +1,13 @@
 package com.board.mapper;
 
+import java.util.List;
+
 import com.board.domain.BoardVO;
-import org.apache.ibatis.annotations.Param;
 
 public interface BoardMapper {
-    public void boardInsert(BoardVO board)throws Exception; 
+	//글작성
+    public void boardInsert(BoardVO board)throws Exception;
+    
+    //글목록
+    public List<BoardVO> boardList() throws Exception; //추가
 }
